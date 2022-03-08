f377f759ff9bde15baab6feb1379db854c41effc
xeropine <susanshrestha783@gmail.com>
2022-03-08 20:33:32 +0545

    fixed player jumping bug

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,13 @@
     public class PlayerMovement : MonoBehaviour
     {
         private Rigidbody2D rb;
+        private BoxCollider2D coll;
         private Animator anim;
-        private float dirX = 0f;
         private SpriteRenderer spr;
+
+        [SerializeField] private LayerMask jumpableGround;
+        
+        private float dirX = 0f;
         [SerializeField] private float moveSpeed = 7f;
         [SerializeField] private float jumpForce = 14f;
 
@@ -19,6 +23,7 @@
             rb = GetComponent<Rigidbody2D>();
             spr = GetComponent<SpriteRenderer>();
             anim = GetComponent<Animator>();
+            coll = GetComponent<BoxCollider2D>();
         }
 
         // Update is called once per frame
@@ -27,7 +32,7 @@
             dirX = Input.GetAxisRaw("Horizontal");
             rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
             
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump") && IsGrounded())
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             }
@@ -35,7 +40,7 @@
         }
 
         private void UpdateAnimationState()
-    {
+        {
         MovementState state;
 
         if (dirX > 0f)
@@ -63,6 +68,11 @@
         }
 
         anim.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 
     }
